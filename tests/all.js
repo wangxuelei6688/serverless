3046b5e4c44f30b4d691ce7246012007e172ff38
ac360 <austen@serverless.com>
2016-09-18 16:06:28 -0700

    add init command for easy onboarding

--- tests/all.js
+++ tests/all.js
@@ -11,6 +11,7 @@ require('./classes/YamlParser');
 require('./classes/CLI');
 
 // Core Plugins Tests
+require('../lib/plugins/create/tests/init');
 require('../lib/plugins/create/tests/create');
 require('../lib/plugins/deploy/tests/deploy');
 require('../lib/plugins/info/tests/info');
